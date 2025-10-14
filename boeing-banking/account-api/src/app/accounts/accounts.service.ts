24a5d88ee09d0bf5582ebe33fccd958857b72f12
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-14 12:33:30 +0530

    updated

--- boeing-banking/account-api/src/app/accounts/accounts.service.ts
+++ boeing-banking/account-api/src/app/accounts/accounts.service.ts
@@ -3,11 +3,17 @@ import { ResponseAccountDto } from './dtos/response-account.dto';
 import { CreateAccountDto } from './dtos/create-account.dto';
 import { UpdateAccountDto } from './dtos/update-account.dto';
 import { IAccountsService } from './accounts.service.interface';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Account, AccountDocument } from './entities/account.entity';
+import { Model } from 'mongoose';
+import { AccountType } from './dtos/create-account.dto';
 @Injectable()
 export class AccountsService implements IAccountsService {
 
-  
+
+  constructor(@InjectModel(Account.name) private readonly accountModel : Model<AccountDocument>) {
+
+  }
 
   findAll(): Promise<ResponseAccountDto[]> {
     // Implementation here
@@ -17,8 +23,18 @@ export class AccountsService implements IAccountsService {
     // Implementation here
   }
 
-  create(data: CreateAccountDto): Promise<ResponseAccountDto> {
+  async create(data: CreateAccountDto): Promise<ResponseAccountDto> {
     // Implementation here
+    const account = new Account();
+    Object.assign(account, data);
+    const createdAccount = new this.accountModel(account);
+    const res = await createdAccount.save();
+    const responseAccountDto = new ResponseAccountDto();
+    responseAccountDto._id = (await res)._id.toString();
+    responseAccountDto.accountType = AccountType[(await res).accountType];
+    responseAccountDto.accountNo = (await res).accountNo;
+    responseAccountDto.runningTotal = (await res).runningTotal;
+    return responseAccountDto;
 
   }
 
