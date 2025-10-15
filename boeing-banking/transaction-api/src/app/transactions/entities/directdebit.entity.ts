5f40a40f32e138508d4b5047f34f3384ae424ccd
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-15 15:01:09 +0530

    updated

--- boeing-banking/transaction-api/src/app/transactions/entities/directdebit.entity.ts
+++ boeing-banking/transaction-api/src/app/transactions/entities/directdebit.entity.ts
@@ -1,5 +1,7 @@
 import { Transaction } from './transaction.entity';
-
+import { ChildEntity, Column } from 'typeorm';
+@ChildEntity("direct_debit")
 export class DirectDebit extends Transaction {
+  @Column({ name: "payment_date", type: "date"})
   paymentDate: Date;
 }
--- boeing-banking/transaction-api/src/app/transactions/entities/externaltransaction.entity.ts
+++ boeing-banking/transaction-api/src/app/transactions/entities/externaltransaction.entity.ts
@@ -1,8 +1,13 @@
+import { ChildEntity, Column } from 'typeorm';
 import { Transaction } from './transaction.entity';
-
+@ChildEntity("external_transactions")
 export class ExternalTransaction extends Transaction{
+  @Column({ name: "bank_name", type: "varchar", length: 100 })
   branchCode: string;
+  @Column({ name: "bank_code", type: "varchar", length: 20 })
   bankCode: string;
+  @Column({ name: "swift_code", type: "varchar", length: 20 } )
   swiftCode: string;
+  @Column({ name: "country", type: "varchar", length: 50 }  )
   country: string;
 }
