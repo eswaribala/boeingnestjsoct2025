86a322e035e5f5093832887a1c55700a15f51f5d
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-16 12:53:23 +0530

    updated

--- userapi/src/users/users.service.ts
+++ userapi/src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserInput, User } from './dto/user.type';
+import { randomUUID } from 'crypto';
+import { faker } from '@faker-js/faker';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    users:User[]=[];
+    create(user:CreateUserInput):User{
+        const newUser:User={
+            userId: faker.number.int({min:1,max:1000}),
+            ...user
+        };
+        this.users.push(newUser);
+        return newUser;
+    }
+
+      
+}
